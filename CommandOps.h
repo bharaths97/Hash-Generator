82efff6a4b98ea6d16ce2585b1f2433035ae6620
Bharath Suresh <bharathsur97@gmail.com>
2020-05-08 23:50:12 +0530

    Files Rearrangement and Renaming

--- CommandOps.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <iostream>
-using namespace std;
-
-void ExtractHash(string buffer, string& out1);
-string ExecuteCommand(string cmd);
-string GetHashofFile(string cmd);
---
+++ Hasher.cpp
@@ -0,0 +1,15 @@
+#include "Hasher.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(cli::array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	HASHER::HashMain form;
+	Application::Run(% form);
+}
